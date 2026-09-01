4bd90b516d12a4dfa9e157bc175ee9ba1a5b10a0
xiaoxiao122627 <3242293698@qq.com>
2026-09-01 18:02:54 +0800

    Add files via upload

---
+++ check_dir.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+# 文件名: check_dir.sh
+# author: Jasyn
+# function: 文件检测脚本：目录不存在则自动创建
+
+
+# 日志配置
+
+LOG_DIR="./logs"
+LOG_FILE="$LOG_DIR/check_dir_$(date +%Y%m%d).log"
+
+mkdir -p "$LOG_DIR"
+
+
+# 日志函数
+
+log() {
+    echo "[$(date '+%Y%m%d %H:%M:%S') $1]" | tee -a "$LOG_FILE"
+}
+
+
+# 输出函数（不带时间戳）
+
+print() {
+    echo "$1" | tee -a "$LOG_FILE"
+}
+
+
+# 调用函数
+
+log "脚本开始执行"
+
+
+# 1. 接收用户输入的目录路径
+read -p "请输入要检测的路径：" dir_path
+log "用户输入路径: $dir_path"
+
+
+# 2. 判断路径是否存在
+if [ -d "$dir_path" ]; then         # -d：测试是否为目录  "$dir_path"：变量值，用双引号包起来防止空格或空值导致语法错误
+        # 存在
+        log "目录 '$dir_path' 已存在"
+	print  "$dir_path' 已存在"
+       
+else
+        # 不存在
+        log "目录 '$dir_path' 不存在，正在创建……"
+	print "目录 '$dir_path' 不存在，正在创建……"
+
+
+        mkdir -p "$dir_path"    # -p: 自动创建路径中所有缺失的父目录
+        # 如果 dir_path 是 /a/b/c，mkdir -p 会先创建 /a，再创建 /a/b，最后创建 /a/b/c；如果不用 -p，而 /a 不存在，就会报错
+        # 创建成功 → 返回 0；失败 → 返回非 0
+        
+        
+        # 3. 检查创建是否成功
+        if [ $? -eq 0 ]; then     # $?:特殊变量，保存上一条命令的退出状态码
+                log "目录创建成功"
+		print "目录创建成功"
+        else
+                log "目录创建失败，请检查权限"
+		print "目录创建失败，请检查权限"
+        fi
+fi
+
+log "脚本执行结束"
